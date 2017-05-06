9e5c8d67210b0c693e0e17021b36564a0c5c72dd
Matt Diephouse <matt@diephouse.com>
2017-05-06 13:08:40 -0400

    Create project

---
+++ Source/PersistDB.h
@@ -0,0 +1,19 @@
+//
+//  PersistDB.h
+//  PersistDB
+//
+//  Created by Matt Diephouse on 5/6/17.
+//  Copyright © 2017 Matt Diephouse. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PersistDB.
+FOUNDATION_EXPORT double PersistDBVersionNumber;
+
+//! Project version string for PersistDB.
+FOUNDATION_EXPORT const unsigned char PersistDBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PersistDB/PublicHeader.h>
+
+
---
+++ Tests/PersistDBTests.swift
@@ -0,0 +1,9 @@
+import XCTest
+@testable import PersistDB
+
+class PersistDBTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+}
