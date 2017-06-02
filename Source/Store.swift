9f71ae507110e1759400ca55116d6d09019723c4
Matt Diephouse <matt@diephouse.com>
2017-06-02 12:17:42 -0400

    Start preliminary Store API

--- Source/Store.swift
+++ Source/Store.swift
@@ -1,4 +1,17 @@
 import Foundation
+import Result
+import Schemata
 
 public final class Store {
+    init() {
+    }
+    
+    init(at url: URL) {
+    }
+}
+
+extension Store {
+    func fetch<Model: RecordObject, Value>(_ projection: Projection<Model, Value>) -> Result<Value, NoError> {
+        fatalError()
+    }
 }
