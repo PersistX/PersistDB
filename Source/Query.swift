8615dd3dfb6e6a0321f040c80599bd0c9b7346e0
Matt Diephouse <matt@diephouse.com>
2017-06-03 22:15:34 -0500

    Begin filling in Query

--- Source/Query.swift
+++ Source/Query.swift
@@ -1,5 +1,19 @@
 import Foundation
 import Schemata
 
-struct Query<Model: RecordModel> {
+/// A value representing a query of a model type.
+public struct Query<Model: RecordModel> {
+    /// Creates a query that returns all instances of `Model` in the store.
+    public init() {
+    }
+}
+
+extension Query: Hashable {
+    public var hashValue: Int {
+        return 0
+    }
+    
+    public static func ==(lhs: Query, rhs: Query) -> Bool {
+        return true
+    }
 }
