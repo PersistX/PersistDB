5fdc7d964b10c234e52c20bd9db5e26b5a676eef
Matt Diephouse <matt@diephouse.com>
2017-06-03 22:27:46 -0500

    Add filter to Query

--- Source/Query.swift
+++ Source/Query.swift
@@ -3,6 +3,10 @@ import Schemata
 
 /// A value representing a query of a model type.
 public struct Query<Model: RecordModel> {
+    /// The predicate used to filter results, or `nil` if the query isn't
+    /// filtered.
+    public var predicate: Predicate<Model>?
+    
     /// Creates a query that returns all instances of `Model` in the store.
     public init() {
     }
@@ -10,10 +14,19 @@ public struct Query<Model: RecordModel> {
 
 extension Query: Hashable {
     public var hashValue: Int {
-        return 0
+        return predicate?.hashValue ?? 0
     }
     
     public static func ==(lhs: Query, rhs: Query) -> Bool {
-        return true
+        return lhs.predicate == rhs.predicate
+    }
+}
+
+extension Query {
+    /// Returns a query that is filtered by the given predicate.
+    public func filter(_ predicate: Predicate<Model>) -> Query {
+        var result = self
+        result.predicate = result.predicate.map { $0 && predicate } ?? predicate
+        return result
     }
 }
