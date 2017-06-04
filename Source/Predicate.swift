53ef8ddf442d090b2c34257ee76c1a15fb230111
Matt Diephouse <matt@diephouse.com>
2017-06-03 22:19:58 -0500

    Predicate should be Hashable

--- Source/Predicate.swift
+++ Source/Predicate.swift
@@ -4,3 +4,13 @@ import Schemata
 public struct Predicate<Model: RecordModel> {
 
 }
+
+extension Predicate: Hashable {
+    public var hashValue: Int {
+        return 0
+    }
+    
+    public static func ==(lhs: Predicate, rhs: Predicate) -> Bool {
+        return true
+    }
+}
