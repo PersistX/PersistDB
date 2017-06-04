d7b11526299e313f75f3c4da895ae27b31a905be
Matt Diephouse <matt@diephouse.com>
2017-06-03 22:40:39 -0500

    Use an array for predicates in a query

--- Source/Query.swift
+++ Source/Query.swift
@@ -3,22 +3,22 @@ import Schemata
 
 /// A value representing a query of a model type.
 public struct Query<Model: RecordModel> {
-    /// The predicate used to filter results, or `nil` if the query isn't
-    /// filtered.
-    public var predicate: Predicate<Model>?
+    /// The predicates used to filter results.
+    public var predicates: [Predicate<Model>]
     
     /// Creates a query that returns all instances of `Model` in the store.
     public init() {
+        predicates = []
     }
 }
 
 extension Query: Hashable {
     public var hashValue: Int {
-        return predicate?.hashValue ?? 0
+        return predicates.map { $0.hashValue }.reduce(0, ^)
     }
     
     public static func ==(lhs: Query, rhs: Query) -> Bool {
-        return lhs.predicate == rhs.predicate
+        return lhs.predicates == rhs.predicates
     }
 }
 
@@ -26,7 +26,7 @@ extension Query {
     /// Return the results of the query as executed against the given models.
     public func evaluate<C: Collection>(_ models: C) -> [Model] where C.Element == Model {
         var result = Array(models)
-        if let predicate = predicate {
+        for predicate in predicates {
             result = result.filter(predicate.evaluate)
         }
         return result
@@ -37,7 +37,7 @@ extension Query {
     /// Returns a query that is filtered by the given predicate.
     public func filter(_ predicate: Predicate<Model>) -> Query {
         var result = self
-        result.predicate = result.predicate.map { $0 && predicate } ?? predicate
+        result.predicates.append(predicate)
         return result
     }
 }
