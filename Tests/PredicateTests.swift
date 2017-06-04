e34a6108d947e5993d0bcfaf8627db951b0d8d38
Matt Diephouse <matt@diephouse.com>
2017-06-03 22:41:09 -0700

    Test Predicate.evaluate with to-one relationship

--- Tests/PredicateTests.swift
+++ Tests/PredicateTests.swift
@@ -7,6 +7,12 @@ private let jkRowling = Author(
     books: Set()
 )
 
+private let sorcerersStone = Book(
+    id: Book.ID("1"),
+    title: "Harry Potter and the Sorcerer's Stone",
+    author: jkRowling
+)
+
 class PredicateTests: XCTestCase {
     func test_evaluate_equal_string_true() {
         let predicate = \Author.name == "J.K. Rowling"
@@ -16,5 +22,15 @@ class PredicateTests: XCTestCase {
     func test_evaluate_equal_string_false() {
         let predicate = \Author.name == "Ray Bradbury"
         XCTAssertFalse(predicate.evaluate(jkRowling))
+    }
+    
+    func test_evaluate_equal_toOne_string_true() {
+        let predicate = \Book.author.name == "J.K. Rowling"
+        XCTAssertTrue(predicate.evaluate(sorcerersStone))
+    }
+    
+    func test_evaluate_equal_toOne_string_false() {
+        let predicate = \Book.author.name == "Ray Bradbury"
+        XCTAssertFalse(predicate.evaluate(sorcerersStone))
     }
 }
