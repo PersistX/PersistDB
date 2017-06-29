62b61e9c1a82739a6016277a8dcbd5ad9e1d4e0a
Matt Diephouse <matt@diephouse.com>
2017-06-29 00:45:48 -0400

    Add basic SQL.Schema creation for Models

--- Source/Model.swift
+++ Source/Model.swift
@@ -1,6 +1,40 @@
 import Foundation
 import Schemata
 
+extension AnyProperty {
+    internal var sql: SQL.Schema.Column? {
+        let dataType: SQL.Schema.DataType?
+        let nullable: Bool
+        switch type {
+        case .toMany:
+            dataType = nil
+            nullable = false
+            
+        case .toOne:
+            dataType = .integer
+            nullable = false
+            
+        case let .value(type, null):
+            let encoded = type.anyValue.encoded
+            if encoded == String.self {
+                dataType = .text
+            } else if encoded == Int.self {
+                dataType = .integer
+            } else {
+                fatalError("Unknown encoded property type \(encoded)")
+            }
+            nullable = null
+        }
+        
+        return dataType.map { SQL.Schema.Column(name: path, type: $0, nullable: nullable) }
+    }
+}
+
 extension Model {
-    
+    internal static var sql: SQL.Schema {
+        return SQL.Schema(
+            table: SQL.Table(String(describing: Self.self)),
+            columns: Set(anySchema.properties.values.flatMap { $0.sql })
+        )
+    }
 }
--- Tests/ModelTests.swift
+++ Tests/ModelTests.swift
@@ -1,6 +1,47 @@
-import PersistDB
+@testable import PersistDB
 import XCTest
 
-class ModelTests: XCTestCase {
+class AnyPropertyTests: XCTestCase {
+    func testSQLForToMany() {
+        let property = Author.anySchema.properties[\Author.books]!
+        XCTAssertNil(property.sql)
+    }
     
+    func testSQLForToOne() {
+        let property = Book.anySchema.properties[\Book.author]!
+        let expected = SQL.Schema.Column(name: "author", type: .integer)
+        XCTAssertEqual(property.sql, expected)
+    }
+    
+    func testSQLForString() {
+        let property = Book.anySchema.properties[\Book.title]!
+        let expected = SQL.Schema.Column(name: "title", type: .text)
+        XCTAssertEqual(property.sql, expected)
+    }
+    
+    func testSQLForInt() {
+        let property = Author.anySchema.properties[\Author.born]!
+        let expected = SQL.Schema.Column(name: "born", type: .integer)
+        XCTAssertEqual(property.sql, expected)
+    }
+    
+    func testSQLForOptionalInt() {
+        let property = Author.anySchema.properties[\Author.died]!
+        let expected = SQL.Schema.Column(name: "died", type: .integer, nullable: true)
+        XCTAssertEqual(property.sql, expected)
+    }
+}
+
+class ModelTests: XCTestCase {
+    func testSQLForBook() {
+        let expected = SQL.Schema(
+            table: SQL.Table("Book"),
+            columns: [
+                .init(name: "id", type: .integer),
+                .init(name: "title", type: .text),
+                .init(name: "author", type: .integer),
+            ]
+        )
+        XCTAssertEqual(Book.sql, expected)
+    }
 }
