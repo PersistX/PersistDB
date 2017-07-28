3bc3f39673437a208d8d1d2e5adc9590cef6b3ad
Matt Diephouse <matt@diephouse.com>
2017-07-27 22:08:12 -0400

    Use generated init

--- PersistTest/Value.swift
+++ PersistTest/Value.swift
@@ -5,14 +5,6 @@ extension TestStore {
     public struct Value<Model: PersistDB.Model> {
         internal let keyPath: PartialKeyPath<Model>
         internal let value: Primitive
-        
-        internal init<Value: ModelValue>(
-            keyPath: KeyPath<Model, Value>,
-            value: Value
-        ) {
-            self.keyPath = keyPath
-            self.value = Value.anyValue.encode(value)
-        }
     }
 }
 
@@ -20,6 +12,9 @@ public func ==<Model: PersistDB.Model, Value: ModelValue>(
     keyPath: KeyPath<Model, Value>,
     value: Value
 ) -> TestStore.Value<Model> {
-    return TestStore.Value(keyPath: keyPath, value: value)
+    return TestStore.Value(
+        keyPath: keyPath,
+        value: Value.anyValue.encode(value)
+    )
 }
 
--- Tests/PersistTest/ValueTests.swift
+++ Tests/PersistTest/ValueTests.swift
@@ -3,12 +3,6 @@ import Schemata
 import XCTest
 
 class TestStoreValueTests: XCTestCase {
-    func testInitWithKeyPathValueSavesInfo() {
-        let value = TestStore.Value(keyPath: \Book.title, value: "Ubik")
-        XCTAssertEqual(value.keyPath, \Book.title)
-        XCTAssertEqual(value.value, Primitive.string("Ubik"))
-    }
-    
     func testEqualsCreatesValue() {
         let value: TestStore.Value = \Book.title == "Ubik"
         XCTAssertEqual(value.keyPath, \Book.title)
