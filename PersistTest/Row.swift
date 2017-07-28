e4aabe95b0dfddebbbd0f0c06cff93e677f8bc2c
Matt Diephouse <matt@diephouse.com>
2017-07-28 08:57:25 -0400

    Implement TestStore.Row

--- PersistTest/Row.swift
+++ PersistTest/Row.swift
@@ -1,7 +1,49 @@
-import Foundation
+import PersistDB
+import Schemata
 
 extension TestStore {
     public struct Row {
-        
+        internal let type: Any.Type
+        internal let id: Primitive
+        internal let values: [AnyValue]
     }
 }
+
+extension TestStore.Row: Hashable {
+    public var hashValue: Int {
+        return ObjectIdentifier(type).hashValue ^ id.hashValue
+    }
+    
+    public static func == (lhs: TestStore.Row, rhs: TestStore.Row) -> Bool {
+        return lhs.type == rhs.type
+            && lhs.id == rhs.id
+            && lhs.values == rhs.values
+    }
+}
+
+infix operator -- : AssignmentPrecedence
+
+public func --<M>(lhs: M.ID, values: [TestStore.Value<M>]) -> TestStore.Row {
+    return TestStore.Row(
+        type: M.self,
+        id: M.ID.anyValue.encode(lhs),
+        values: values.map(TestStore.AnyValue.init)
+    )
+}
+
+public func --<M>(lhs: M.ID, value: TestStore.Value<M>) -> TestStore.Row {
+    return TestStore.Row(
+        type: M.self,
+        id: M.ID.anyValue.encode(lhs),
+        values: [TestStore.AnyValue(value)]
+    )
+}
+
+public func --<M: PersistDB.Model>(lhs: M.ID, type: M.Type) -> TestStore.Row {
+    return TestStore.Row(
+        type: M.self,
+        id: M.ID.anyValue.encode(lhs),
+        values: []
+    )
+}
+
--- Tests/PersistTest/RowTests.swift
+++ Tests/PersistTest/RowTests.swift
@@ -1,5 +1,54 @@
-import PersistDB
+@testable import PersistTest
 import XCTest
 
-class TestDBRowTests: XCTestCase {
+class TestStoreRowTests: XCTestCase {
+    func testInitWithArrayOfValues() {
+        let id = Author.ID(1)
+        let row = id -- [
+            \Author.name == "Ray Bradbury",
+            \Author.born == 1920,
+            \Author.died == 2012,
+        ]
+        
+        let expected = TestStore.Row(
+            type: Author.self,
+            id: .int(1),
+            values: [
+                TestStore.AnyValue(keyPath: \Author.name, value: .string("Ray Bradbury")),
+                TestStore.AnyValue(keyPath: \Author.born, value: .int(1920)),
+                TestStore.AnyValue(keyPath: \Author.died, value: .int(2012)),
+            ]
+        )
+        
+        XCTAssertEqual(row, expected)
+    }
+    
+    func testInitWithSingleValue() {
+        let id = Author.ID(1)
+        let row = id -- \Author.name == "Ray Bradbury"
+        
+        let expected = TestStore.Row(
+            type: Author.self,
+            id: .int(1),
+            values: [
+                TestStore.AnyValue(keyPath: \Author.name, value: .string("Ray Bradbury")),
+            ]
+        )
+        
+        XCTAssertEqual(row, expected)
+        
+    }
+    
+    func testInitWithClass() {
+        let id = Author.ID(1)
+        let row = id -- Author.self
+        
+        let expected = TestStore.Row(
+            type: Author.self,
+            id: .int(1),
+            values: [ ]
+        )
+        
+        XCTAssertEqual(row, expected)
+    }
 }
