7f1263573579a99e94f39ef0a7dc6391e55cfc20
Matt Diephouse <matt@diephouse.com>
2017-07-27 21:53:35 -0400

    Fill in TestDB.Value

--- PersistTest/Value.swift
+++ PersistTest/Value.swift
@@ -1,7 +1,25 @@
-import Foundation
+import PersistDB
+import Schemata
 
 extension TestDB {
     public struct Value {
+        internal let keyPath: AnyKeyPath
+        internal let value: Primitive
         
+        internal init<Model: PersistDB.Model, Value: ModelValue>(
+            keyPath: KeyPath<Model, Value>,
+            value: Value
+        ) {
+            self.keyPath = keyPath
+            self.value = Value.anyValue.encode(value)
+        }
     }
 }
+
+public func ==<Model: PersistDB.Model, Value: ModelValue>(
+    keyPath: KeyPath<Model, Value>,
+    value: Value
+) -> TestDB.Value {
+    return TestDB.Value(keyPath: keyPath, value: value)
+}
+
--- Tests/PersistTest/ValueTests.swift
+++ Tests/PersistTest/ValueTests.swift
@@ -1,6 +1,18 @@
-import PersistDB
+@testable import PersistTest
+import Schemata
 import XCTest
 
 class TestDBValueTests: XCTestCase {
+    func testInitWithKeyPathValueSavesInfo() {
+        let value = PersistTest.TestDB.Value(keyPath: \Book.title, value: "Ubik")
+        XCTAssertEqual(value.keyPath, \Book.title)
+        XCTAssertEqual(value.value, Primitive.string("Ubik"))
+    }
+    
+    func testEqualsCreatesValue() {
+        let value: PersistTest.TestDB.Value = \Book.title == "Ubik"
+        XCTAssertEqual(value.keyPath, \Book.title)
+        XCTAssertEqual(value.value, .string("Ubik"))
+    }
 }
 
