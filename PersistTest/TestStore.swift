fd49f3afd2646723b8edc481503321fb958ac23d
Matt Diephouse <matt@diephouse.com>
2017-07-31 08:28:01 -0400

    Add an init to TestStore

--- PersistTest/TestStore.swift
+++ PersistTest/TestStore.swift
@@ -1,5 +1,25 @@
 import PersistDB
+import Schemata
 
 public final class TestStore {
+    internal struct ID {
+        let type: Any.Type
+        let id: Primitive
+    }
     
+    internal let data: [ID: [AnyValue]]
+    
+    public init(_ rows: Row...) {
+        data = Dictionary(uniqueKeysWithValues: rows.map { (ID(type: $0.type, id: $0.id), $0.values) })
+    }
+}
+
+extension TestStore.ID: Hashable {
+    var hashValue: Int {
+        return ObjectIdentifier(type).hashValue ^ id.hashValue
+    }
+    
+    static func ==(lhs: TestStore.ID, rhs: TestStore.ID) -> Bool {
+        return lhs.type == rhs.type && lhs.id == rhs.id
+    }
 }
