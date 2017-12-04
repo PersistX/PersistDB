d3e96885382e2a07bb893b10292c21f9bb399310
Matt Diephouse <matt@diephouse.com>
2017-12-04 09:35:47 -0500

    Delete PersistTest

--- PersistTest/PersistTest.h
+++
@@ -1,7 +0,0 @@
-#import <Cocoa/Cocoa.h>
-
-//! Project version number for PersistTest.
-FOUNDATION_EXPORT double PersistTestVersionNumber;
-
-//! Project version string for PersistTest.
-FOUNDATION_EXPORT const unsigned char PersistTestVersionString[];
