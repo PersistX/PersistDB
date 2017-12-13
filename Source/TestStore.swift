4a25a3bc4939a0bbf9d3701ae976822c587816c1
Matt Diephouse <matt@diephouse.com>
2017-12-13 13:26:14 -0500

    Use Insert in TestStore

--- Source/TestStore.swift
+++ Source/TestStore.swift
@@ -9,17 +9,7 @@ private struct ID<A: PersistDB.Model>: ModelProjection {
     let id: Model.ID
     
     static var projection: Projection<Model, ID<Model>> {
-        let id = Model.schema.properties.values.first { $0.path == "id" }!.keyPath as! KeyPath<Model, Model.ID>
-        return Projection<Model, ID<Model>>(ID.init, id)
-    }
-}
-
-extension SQL.Insert {
-    fileprivate init(_ row: TestStore.Row) {
-        self.init(
-            table: row.table,
-            values: row.values
-        )
+        return Projection<Model, ID<Model>>(ID.init, Model.idKeyPath)
     }
 }
 
@@ -39,41 +29,29 @@ extension Schemata.AnyModel {
     }
 }
 
-public final class TestStore {
-    fileprivate struct Row {
-        let table: SQL.Table
-        let values: [String: SQL.Expression]
-        
-        init<Model>(_ id: Model.ID, _ valueSet: ValueSet<Model>) {
-            let schema = Model.schema
-            let idValue = SQL.Expression.value(Model.ID.anyValue.encode(id).sql)
-            let pairs = [("id", idValue)] + valueSet.assignments.map { assignment -> (String, SQL.Expression) in
-                let column = schema.properties(for: assignment.keyPath).last!.path
-                return (column, assignment.sql)
-            }
-            table = SQL.Table(String(describing: Model.self))
-            values = Dictionary(uniqueKeysWithValues: pairs)
-        }
+extension Insert {
+    fileprivate init(_ id: Model.ID, _ valueSet: ValueSet<Model>) {
+        let idValue = SQL.Expression.value(Model.ID.anyValue.encode(id).sql)
+        let id = Assignment(keyPath: Model.idKeyPath, sql: idValue)
+        var values = valueSet
+        values.assignments.insert(id, at: 0)
+        self.init(unvalidated: values)
     }
-    
+}
+
+public final class TestStore {
     let store: Store
     
-    private init(
-        types: [Schemata.AnyModel.Type],
-        inserts: [SQL.Insert]
-    ) {
+    private init(for types: [Schemata.AnyModel.Type]) {
         store = Store(for: types.map { $0.testSchema })
-        inserts.forEach(store.db.insert)
     }
     
     public convenience init<A>(
         _ a: [A.ID: ValueSet<A>]
     ) {
-        let aRows = a.map(Row.init)
-        self.init(
-            types: [A.self],
-            inserts: aRows.map(SQL.Insert.init)
-        )
+        self.init(for: [A.self])
+        let aRows = a.map(Insert.init)
+        aRows.forEach(store.insert)
     }
     
     public func fetch<Model>(
