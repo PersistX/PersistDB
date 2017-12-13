5b0900aa823834655accc4f2ed81edb496e85df7
Matt Diephouse <matt@diephouse.com>
2017-12-13 13:25:30 -0500

    Add Insert

---
+++ Source/Insert.swift
@@ -0,0 +1,38 @@
+public struct Insert<Model: PersistDB.Model> {
+    internal let valueSet: ValueSet<Model>
+    
+    public init(_ valueSet: ValueSet<Model>) {
+        precondition(valueSet.sufficientForInsert)
+        self.valueSet = valueSet
+    }
+    
+    internal init(unvalidated valueSet: ValueSet<Model>) {
+        self.valueSet = valueSet
+    }
+}
+
+extension Insert: Hashable {
+    public var hashValue: Int {
+        return valueSet.hashValue
+    }
+    
+    public static func == (lhs: Insert, rhs: Insert) -> Bool {
+        return lhs.valueSet == rhs.valueSet
+    }
+}
+
+extension Insert {
+    internal var sql: SQL.Insert {
+        let table = SQL.Table(Model.schema.name)
+        let values = valueSet
+            .assignments
+            .map { assignment -> (String, SQL.Expression) in
+                let path = Model.schema.properties[assignment.keyPath]!.path
+                return (path, assignment.sql)
+            }
+        return SQL.Insert(
+            table: table,
+            values: Dictionary(uniqueKeysWithValues: values)
+        )
+    }
+}
