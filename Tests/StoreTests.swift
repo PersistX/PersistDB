17ad97530feaa942e0147bdef8943940e894758f
Matt Diephouse <matt@diephouse.com>
2017-12-14 11:03:47 -0500

    Test roundtrip insert-->fetch

--- Tests/StoreTests.swift
+++ Tests/StoreTests.swift
@@ -1,5 +1,47 @@
+import PersistDB
 import Schemata
 import XCTest
 
+private let fixtures: [AnyModel.Type] = [Author.self, Book.self]
+
+private struct AuthorInfo {
+    let id: Author.ID
+    let name: String
+    let born: Int
+    let died: Int?
+}
+
+extension AuthorInfo: ModelProjection {
+    static let projection = Projection<Author, AuthorInfo>(
+        AuthorInfo.init,
+        \.id,
+        \.name,
+        \.born,
+        \.died
+    )
+}
+
 class StoreTests: XCTestCase {
+    func testInsertFetch() {
+        let store = Store(for: fixtures)
+        
+        let author = Author.jrrTolkien
+        let insert: Insert<Author> = [
+            \Author.id == author.id,
+            \Author.name == author.name,
+            \Author.born == author.born,
+            \Author.died == author.died,
+        ]
+        
+        store.insert(insert)
+        let info: AuthorInfo = store
+            .fetch(Author.all)
+            .first()!
+            .value!
+        
+        XCTAssertEqual(info.id, author.id)
+        XCTAssertEqual(info.name, author.name)
+        XCTAssertEqual(info.born, author.born)
+        XCTAssertEqual(info.died, author.died)
+    }
 }
