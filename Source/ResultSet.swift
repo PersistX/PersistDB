b9c3ee23c773cda4113f309a9319cc99f60301ed
Matt Diephouse <matt@diephouse.com>
2018-01-20 21:10:19 -0500

    Implement ResultSet diffing

--- Source/ResultSet.swift
+++ Source/ResultSet.swift
@@ -1,3 +1,4 @@
+import Differ
 import Schemata
 
 /// An ordered set of results from a query.
@@ -8,7 +9,7 @@ public struct ResultSet<Key: ModelValue, Projection: PersistDB.ModelProjection> 
     public private(set) var groups: [Group<Key, Projection>]
 
     /// All the values from all the groups in the set.
-    private var values: [Projection]
+    fileprivate var values: [Projection]
 
     /// Create an empty result set.
     public init() {
@@ -21,6 +22,13 @@ public struct ResultSet<Key: ModelValue, Projection: PersistDB.ModelProjection> 
         values = groups.flatMap { $0.values }
         precondition(Set(groups.map { $0.key }).count == groups.count)
         precondition(Set(values).count == values.count)
+    }
+}
+
+extension ResultSet {
+    /// All the keys from all the groups.
+    public var keys: [Key] {
+        return groups.map { $0.key }
     }
 }
 
@@ -56,5 +64,195 @@ extension ResultSet: Collection {
 
     public func index(after i: Index) -> Index {
         return values.index(after: i)
+    }
+}
+
+private func diff<K, P>(
+    from old: ResultSet<K, P>,
+    to new: ResultSet<K, P>
+) -> ResultSet<K, P>.Diff {
+    var diff = ResultSet<K, P>.Diff()
+
+    func indicesByID(in resultSet: ResultSet<K, P>) -> [P.Model.ID: (Int, Int)] {
+        var result: [P.Model.ID: (Int, Int)] = [:]
+        for g in resultSet.groups.indices {
+            let group = resultSet.groups[g]
+            for v in group.values.indices {
+                let value = group.values[v]
+                result[value.id] = (g, v)
+            }
+        }
+        return result
+    }
+
+    let oldIndicesByID = indicesByID(in: old)
+    let newIndicesByID = indicesByID(in: new)
+
+    // 1. Diff the group keys by themselves.
+    //
+    // This generates all `deleteGroup`, `insertGroup`, and `moveGroup`s.
+    let groupsDiff = old.keys.extendedDiff(new.keys)
+    let groupDeltas = groupsDiff
+        .elements
+        .map { element -> ResultSet<K, P>.Diff.Delta in
+            switch element {
+            case let .insert(index):
+                return .insertGroup(index)
+            case let .delete(index):
+                return .deleteGroup(index)
+            case let .move(from, to):
+                return .moveGroup(from, to)
+            }
+        }
+    diff.deltas.formUnion(groupDeltas)
+    dump(groupDeltas)
+
+    // 2. Diff the values by themselves.
+    var oldGroups = old.groups
+    for case let .move(from, to) in groupsDiff.patch(from: old.keys, to: new.keys) {
+        let group = oldGroups.remove(at: from)
+        oldGroups.insert(group, at: to)
+    }
+    let oldValues = oldGroups.flatMap { $0.values }
+    let newValues = new.values
+    let valueElements = oldValues.extendedDiff(newValues).elements
+    let moveDeltas = valueElements.flatMap { element -> ResultSet<K, P>.Diff.Delta? in
+        guard case let .move(from, _) = element else { return nil }
+        let id = old[from].id
+        let old = oldIndicesByID[id]!
+        let new = newIndicesByID[id]!
+        return .updateValue(old.0, old.1, new.0, new.1)
+    }
+    diff.deltas.formUnion(moveDeltas)
+    dump(moveDeltas)
+
+    // 3. Find all inserted, deleted, and changed values by ID
+    let addedIDs = Set(valueElements.flatMap { element -> P.Model.ID? in
+        guard case let .insert(index) = element else { return nil }
+        return new[index].id
+    })
+    let removedIDs = Set(valueElements.flatMap { element -> P.Model.ID? in
+        guard case let .delete(index) = element else { return nil }
+        return old[index].id
+    })
+    let updatedIDs = addedIDs.intersection(removedIDs)
+    let insertedIDs = addedIDs.subtracting(updatedIDs)
+    let deletedIDs = removedIDs.subtracting(updatedIDs)
+    let inserted = insertedIDs
+        .map { newIndicesByID[$0]! }
+        .map(ResultSet<K, P>.Diff.Delta.insertValue)
+    let deleted = deletedIDs
+        .map { oldIndicesByID[$0]! }
+        .map(ResultSet<K, P>.Diff.Delta.deleteValue)
+    let updated = updatedIDs.map { id -> ResultSet<K, P>.Diff.Delta in
+        let old = oldIndicesByID[id]!
+        let new = newIndicesByID[id]!
+        return ResultSet<K, P>.Diff.Delta.updateValue(old.0, old.1, new.0, new.1)
+    }
+    diff.deltas.formUnion(inserted)
+    diff.deltas.formUnion(deleted)
+    diff.deltas.formUnion(updated)
+
+    // 4. Check the group boundaries to find additional moves
+    let shifted = Set(new.map { $0.id })
+        .subtracting(insertedIDs)
+        .subtracting(updatedIDs)
+        .flatMap { id -> ResultSet<K, P>.Diff.Delta? in
+            let oldIndex = oldIndicesByID[id]!
+            let newIndex = newIndicesByID[id]!
+            let oldKey = old.groups[oldIndex.0].key
+            let newKey = new.groups[newIndex.0].key
+            if oldKey == newKey { return nil }
+            return .updateValue(oldIndex.0, oldIndex.1, newIndex.0, newIndex.1)
+        }
+    diff.deltas.formUnion(shifted)
+
+    return diff
+}
+
+extension ResultSet {
+    /// The difference between two result sets.
+    public struct Diff {
+        /// A change within a diff.
+        public enum Delta {
+            /// The group at the given index in the old set was deleted.
+            ///
+            /// Values within the group are assumed to be deleted unless in a `.updateValue`.
+            case deleteGroup(Int)
+            /// The group at the given index in the new set was inserted.
+            case insertGroup(Int)
+            /// The group was moved from the given index in the old set to the given index in the
+            /// new set.
+            case moveGroup(Int, Int)
+            /// The value at the given group and value indices in the old set was deleted.
+            case deleteValue(Int, Int)
+            /// The value at the given group and value indices in the new set was inserted.
+            case insertValue(Int, Int)
+            /// The values was moved from the given group and value indices in the old set to the
+            /// given group and value indices in the new set.
+            case updateValue(Int, Int, Int, Int)
+        }
+
+        /// The changes that make up the diff.
+        public var deltas: Set<Delta>
+
+        /// Create an empty diff.
+        public init() {
+            deltas = []
+        }
+
+        /// Create a diff with the given deltas.
+        public init(_ deltas: Set<Delta>) {
+            self.deltas = deltas
+        }
+    }
+
+    /// Calculate the diff from `resultSet` to `self`.
+    public func diff(from resultSet: ResultSet) -> Diff {
+        return PersistDB.diff(from: resultSet, to: self)
+    }
+}
+
+extension ResultSet.Diff.Delta: Hashable {
+    public var hashValue: Int {
+        switch self {
+        case let .deleteGroup(i),
+             let .insertGroup(i):
+            return i
+        case let .moveGroup(g1, g2):
+            return g1 ^ g2
+        case let .deleteValue(g, v),
+             let .insertValue(g, v):
+            return g ^ v
+        case let .updateValue(g1, v1, g2, v2):
+            return g1 ^ v1 ^ g2 ^ v2
+        }
+    }
+
+    public static func == (lhs: ResultSet.Diff.Delta, rhs: ResultSet.Diff.Delta) -> Bool {
+        switch (lhs, rhs) {
+        case let (.deleteGroup(lhs), .deleteGroup(rhs)),
+             let (.insertGroup(lhs), .insertGroup(rhs)):
+            return lhs == rhs
+        case let (.moveGroup(lhs), .moveGroup(rhs)):
+            return lhs == rhs
+        case let (.deleteValue(lhs), .deleteValue(rhs)),
+             let (.insertValue(lhs), .insertValue(rhs)):
+            return lhs == rhs
+        case let (.updateValue(lhs), .updateValue(rhs)):
+            return lhs == rhs
+        default:
+            return false
+        }
+    }
+}
+
+extension ResultSet.Diff: Hashable {
+    public var hashValue: Int {
+        return deltas.map { $0.hashValue }.reduce(0, ^)
+    }
+
+    public static func == (lhs: ResultSet.Diff, rhs: ResultSet.Diff) -> Bool {
+        return lhs.deltas == rhs.deltas
     }
 }
--- Tests/ResultSetTests.swift
+++ Tests/ResultSetTests.swift
@@ -2,14 +2,12 @@
 import Schemata
 import XCTest
 
-private let groupedEmpty = ResultSet<Int, AuthorInfo>()
 private let grouped = ResultSet<Int, AuthorInfo>([
     Group(key: 1892, values: [AuthorInfo(.jrrTolkien)]),
     Group(key: 1920, values: [AuthorInfo(.isaacAsimov), AuthorInfo(.rayBradbury)]),
     Group(key: 1951, values: [AuthorInfo(.orsonScottCard)]),
 ])
 
-private let ungroupedEmpty = ResultSet<None, AuthorInfo>()
 private let ungrouped = ResultSet<None, AuthorInfo>([
     AuthorInfo(.isaacAsimov),
     AuthorInfo(.jrrTolkien),
@@ -31,7 +29,7 @@ class ResultSetInitProjections: XCTestCase {
 
 class ResultSetCollectionTests: XCTestCase {
     func testCountGroupedEmpty() {
-        XCTAssertEqual(groupedEmpty.count, 0)
+        XCTAssertEqual(ResultSet<Int, AuthorInfo>().count, 0)
     }
 
     func testCountGrouped() {
@@ -39,10 +37,249 @@ class ResultSetCollectionTests: XCTestCase {
     }
 
     func testCountUngroupedEmpty() {
-        XCTAssertEqual(ungroupedEmpty.count, 0)
+        XCTAssertEqual(ResultSet<None, AuthorInfo>().count, 0)
     }
 
     func testCountUngrouped() {
         XCTAssertEqual(ungrouped.count, 4)
+    }
+}
+
+class ResultSetDiffTests: XCTestCase {
+    func testNoDiff() {
+        let actual = grouped.diff(from: grouped)
+        let expected = ResultSet<Int, AuthorInfo>.Diff([])
+        XCTAssertEqual(actual, expected)
+    }
+
+    func testToEmpty() {
+        let actual = ResultSet().diff(from: grouped)
+        let expected = ResultSet<Int, AuthorInfo>.Diff([
+            .deleteGroup(0),
+            .deleteValue(0, 0),
+            .deleteGroup(1),
+            .deleteValue(1, 0),
+            .deleteValue(1, 1),
+            .deleteGroup(2),
+            .deleteValue(2, 0),
+        ])
+        XCTAssertEqual(actual, expected)
+    }
+
+    func testFromEmpty() {
+        let actual = grouped.diff(from: ResultSet())
+        let expected = ResultSet<Int, AuthorInfo>.Diff([
+            .insertGroup(0),
+            .insertValue(0, 0),
+            .insertGroup(1),
+            .insertValue(1, 0),
+            .insertValue(1, 1),
+            .insertGroup(2),
+            .insertValue(2, 0),
+        ])
+        XCTAssertEqual(actual, expected)
+    }
+
+    func testInsertEmptyGroup() {
+        let empty = ResultSet<None, AuthorInfo>([
+            Group(key: .none, values: []),
+        ])
+        let actual = empty.diff(from: ResultSet())
+        let expected = ResultSet<None, AuthorInfo>.Diff([
+            .insertGroup(0),
+        ])
+        XCTAssertEqual(actual, expected)
+    }
+
+    func testDeleteEmptyGroup() {
+        let empty = ResultSet<None, AuthorInfo>([
+            Group(key: .none, values: []),
+        ])
+        let actual = ResultSet().diff(from: empty)
+        let expected = ResultSet<None, AuthorInfo>.Diff([
+            .deleteGroup(0),
+        ])
+        XCTAssertEqual(actual, expected)
+    }
+
+    func testInsertGroup() {
+        let after = ResultSet<Int, AuthorInfo>([
+            Group(key: 1892, values: [AuthorInfo(.jrrTolkien)]),
+            Group(key: 1920, values: [AuthorInfo(.isaacAsimov), AuthorInfo(.rayBradbury)]),
+            Group(key: 1951, values: [AuthorInfo(.orsonScottCard)]),
+            Group(key: 1963, values: [AuthorInfo(.liuCixin)]),
+        ])
+        let actual = after.diff(from: grouped)
+        let expected = ResultSet<Int, AuthorInfo>.Diff([
+            .insertGroup(3),
+            .insertValue(3, 0),
+        ])
+        XCTAssertEqual(actual, expected)
+    }
+
+    func testDeleteGroup() {
+        let after = ResultSet<Int, AuthorInfo>([
+            Group(key: 1892, values: [AuthorInfo(.jrrTolkien)]),
+            Group(key: 1951, values: [AuthorInfo(.orsonScottCard)]),
+        ])
+        let actual = after.diff(from: grouped)
+        let expected = ResultSet<Int, AuthorInfo>.Diff([
+            .deleteGroup(1),
+            .deleteValue(1, 0),
+            .deleteValue(1, 1),
+        ])
+        XCTAssertEqual(actual, expected)
+    }
+
+    func testMoveGroup() {
+        let after = ResultSet<Int, AuthorInfo>([
+            Group(key: 1892, values: [AuthorInfo(.jrrTolkien)]),
+            Group(key: 1951, values: [AuthorInfo(.orsonScottCard)]),
+            Group(key: 1920, values: [AuthorInfo(.isaacAsimov), AuthorInfo(.rayBradbury)]),
+        ])
+        let actual = after.diff(from: grouped)
+        let expected = ResultSet<Int, AuthorInfo>.Diff([
+            .moveGroup(1, 2),
+        ])
+        XCTAssertEqual(actual, expected)
+    }
+
+    func testMoveWithinMoveGroup() {
+        let after = ResultSet<Int, AuthorInfo>([
+            Group(key: 1892, values: [AuthorInfo(.jrrTolkien)]),
+            Group(key: 1951, values: [AuthorInfo(.orsonScottCard)]),
+            Group(key: 1920, values: [AuthorInfo(.rayBradbury), AuthorInfo(.isaacAsimov)]),
+        ])
+        let actual = after.diff(from: grouped)
+        let expected = ResultSet<Int, AuthorInfo>.Diff([
+            .moveGroup(1, 2),
+            .updateValue(1, 1, 2, 0),
+        ])
+        XCTAssertEqual(actual, expected)
+    }
+
+    func testUpdateGroup() {
+        let after = ResultSet<Int, AuthorInfo>([
+            Group(key: 1892, values: [AuthorInfo(.jrrTolkien)]),
+            Group(key: 0, values: [AuthorInfo(.isaacAsimov), AuthorInfo(.rayBradbury)]),
+            Group(key: 1951, values: [AuthorInfo(.orsonScottCard)]),
+        ])
+        let actual = after.diff(from: grouped)
+        let expected = ResultSet<Int, AuthorInfo>.Diff([
+            .deleteGroup(1),
+            .insertGroup(1),
+            .updateValue(1, 0, 1, 0),
+            .updateValue(1, 1, 1, 1),
+        ])
+        XCTAssertEqual(actual, expected)
+    }
+
+    func testInsertValue() {
+        let before = ResultSet<Int, AuthorInfo>([
+            Group(key: 1892, values: [AuthorInfo(.jrrTolkien)]),
+            Group(key: 1920, values: [AuthorInfo(.isaacAsimov)]),
+            Group(key: 1951, values: [AuthorInfo(.orsonScottCard)]),
+        ])
+        let actual = grouped.diff(from: before)
+        let expected = ResultSet<Int, AuthorInfo>.Diff([
+            .insertValue(1, 1),
+        ])
+        XCTAssertEqual(actual, expected)
+    }
+
+    func testDeleteValue() {
+        let after = ResultSet<Int, AuthorInfo>([
+            Group(key: 1892, values: [AuthorInfo(.jrrTolkien)]),
+            Group(key: 1920, values: [AuthorInfo(.isaacAsimov)]),
+            Group(key: 1951, values: [AuthorInfo(.orsonScottCard)]),
+        ])
+        let actual = after.diff(from: grouped)
+        let expected = ResultSet<Int, AuthorInfo>.Diff([
+            .deleteValue(1, 1),
+        ])
+        XCTAssertEqual(actual, expected)
+    }
+
+    func testUpdateValue() {
+        let asimov = AuthorInfo(.isaacAsimov, name: Author.Data.isaacAsimov.givenName)
+
+        let after = ResultSet<Int, AuthorInfo>([
+            Group(key: 1892, values: [AuthorInfo(.jrrTolkien)]),
+            Group(key: 1920, values: [asimov, AuthorInfo(.rayBradbury)]),
+            Group(key: 1951, values: [AuthorInfo(.orsonScottCard)]),
+        ])
+        let actual = after.diff(from: grouped)
+        let expected = ResultSet<Int, AuthorInfo>.Diff([
+            .updateValue(1, 0, 1, 0),
+        ])
+        XCTAssertEqual(actual, expected)
+    }
+
+    func testReplaceValue() {
+        let after = ResultSet<Int, AuthorInfo>([
+            Group(key: 1892, values: [AuthorInfo(.jrrTolkien)]),
+            Group(key: 1920, values: [AuthorInfo(.liuCixin), AuthorInfo(.rayBradbury)]),
+            Group(key: 1951, values: [AuthorInfo(.orsonScottCard)]),
+        ])
+        let actual = after.diff(from: grouped)
+        let expected = ResultSet<Int, AuthorInfo>.Diff([
+            .deleteValue(1, 0),
+            .insertValue(1, 0),
+        ])
+        XCTAssertEqual(actual, expected)
+    }
+
+    func testMoveValueWithinGroup() {
+        let after = ResultSet<Int, AuthorInfo>([
+            Group(key: 1892, values: [AuthorInfo(.jrrTolkien)]),
+            Group(key: 1920, values: [AuthorInfo(.rayBradbury), AuthorInfo(.isaacAsimov)]),
+            Group(key: 1951, values: [AuthorInfo(.orsonScottCard)]),
+        ])
+        let actual = after.diff(from: grouped)
+        let expected = ResultSet<Int, AuthorInfo>.Diff([
+            .updateValue(1, 0, 1, 1),
+        ])
+        XCTAssertEqual(actual, expected)
+    }
+
+    func testMoveValueBetweenGroups() {
+        let after = ResultSet<Int, AuthorInfo>([
+            Group(key: 1892, values: [AuthorInfo(.jrrTolkien)]),
+            Group(key: 1920, values: [AuthorInfo(.isaacAsimov)]),
+            Group(key: 1951, values: [AuthorInfo(.rayBradbury), AuthorInfo(.orsonScottCard)]),
+        ])
+        let actual = after.diff(from: grouped)
+        let expected = ResultSet<Int, AuthorInfo>.Diff([
+            .updateValue(1, 1, 2, 0),
+        ])
+        XCTAssertEqual(actual, expected)
+    }
+
+    func testInsertValueBeforeDelete() {
+        let after = ResultSet<Int, AuthorInfo>([
+            Group(key: 1892, values: [AuthorInfo(.jrrTolkien)]),
+            Group(key: 1920, values: [AuthorInfo(.liuCixin), AuthorInfo(.isaacAsimov)]),
+            Group(key: 1951, values: [AuthorInfo(.orsonScottCard)]),
+        ])
+        let actual = after.diff(from: grouped)
+        let expected = ResultSet<Int, AuthorInfo>.Diff([
+            .insertValue(1, 0),
+            .deleteValue(1, 1),
+        ])
+        XCTAssertEqual(actual, expected)
+    }
+
+    func testDeleteValueBeforeInsert() {
+        let after = ResultSet<Int, AuthorInfo>([
+            Group(key: 1892, values: [AuthorInfo(.jrrTolkien)]),
+            Group(key: 1920, values: [AuthorInfo(.rayBradbury), AuthorInfo(.liuCixin)]),
+            Group(key: 1951, values: [AuthorInfo(.orsonScottCard)]),
+        ])
+        let actual = after.diff(from: grouped)
+        let expected = ResultSet<Int, AuthorInfo>.Diff([
+            .insertValue(1, 1),
+            .deleteValue(1, 0),
+        ])
+        XCTAssertEqual(actual, expected)
     }
 }
