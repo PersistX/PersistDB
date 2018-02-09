c6f9fbb9317f807f71760bb883a13be32eec3ef4
Matt Diephouse <matt@diephouse.com>
2018-02-09 16:06:04 -0500

    Add AnyDelete, a type-erased Delete

--- Source/Delete.swift
+++ Source/Delete.swift
@@ -1,3 +1,36 @@
+import Schemata
+
+/// A type-erased delete action.
+internal struct AnyDelete {
+    internal var schema: AnySchema
+    internal var predicate: AnyExpression?
+
+    internal init(_ schema: AnySchema, _ predicate: AnyExpression?) {
+        self.schema = schema
+        self.predicate = predicate
+    }
+}
+
+extension AnyDelete: Hashable {
+    var hashValue: Int {
+        return (predicate?.hashValue ?? 0)
+    }
+
+    static func == (lhs: AnyDelete, rhs: AnyDelete) -> Bool {
+        return lhs.schema == rhs.schema && lhs.predicate == rhs.predicate
+    }
+}
+
+extension AnyDelete {
+    internal func makeSQL() -> SQL.Delete {
+        return SQL.Delete(
+            table: SQL.Table(schema.name),
+            predicate: predicate?.sql
+        )
+    }
+}
+
+/// An action that deletes model entities.
 public struct Delete<Model: PersistDB.Model> {
     public var predicate: Predicate<Model>?
 
@@ -6,21 +39,24 @@ public struct Delete<Model: PersistDB.Model> {
     }
 }
 
+extension Delete {
+    internal var delete: AnyDelete {
+        return AnyDelete(Model.anySchema, predicate?.expression)
+    }
+}
+
 extension Delete: Hashable {
     public var hashValue: Int {
-        return (predicate?.hashValue ?? 0)
+        return delete.hashValue
     }
 
     public static func == (lhs: Delete, rhs: Delete) -> Bool {
-        return lhs.predicate == rhs.predicate
+        return lhs.delete == rhs.delete
     }
 }
 
 extension Delete {
     internal func makeSQL() -> SQL.Delete {
-        return SQL.Delete(
-            table: SQL.Table(Model.schema.name),
-            predicate: predicate?.expression.sql
-        )
+        return delete.makeSQL()
     }
 }
