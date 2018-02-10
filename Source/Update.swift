2adea0ac9b0205433b4cb944d088f43c3a377d94
Matt Diephouse <matt@diephouse.com>
2018-02-09 19:15:57 -0500

    Add AnyUpdate, a type-erased Update

--- Source/Update.swift
+++ Source/Update.swift
@@ -1,3 +1,35 @@
+import Schemata
+
+/// A type-erased update action
+internal struct AnyUpdate {
+    internal var schema: AnySchema
+    internal var predicate: AnyExpression?
+    internal var valueSet: AnyValueSet
+}
+
+extension AnyUpdate: Hashable {
+    var hashValue: Int {
+        return valueSet.hashValue
+    }
+
+    static func == (lhs: AnyUpdate, rhs: AnyUpdate) -> Bool {
+        return lhs.schema == rhs.schema
+            && lhs.predicate == rhs.predicate
+            && lhs.valueSet == rhs.valueSet
+    }
+}
+
+extension AnyUpdate {
+    internal func makeSQL() -> SQL.Update {
+        return SQL.Update(
+            table: SQL.Table(schema.name),
+            values: valueSet.makeSQL(),
+            predicate: predicate?.sql
+        )
+    }
+}
+
+/// An action that updates model entities.
 public struct Update<Model: PersistDB.Model> {
     public var predicate: Predicate<Model>?
     public let valueSet: ValueSet<Model>
@@ -5,6 +37,16 @@ public struct Update<Model: PersistDB.Model> {
     public init(predicate: Predicate<Model>?, valueSet: ValueSet<Model>) {
         self.predicate = predicate
         self.valueSet = valueSet
+    }
+}
+
+extension Update {
+    internal var update: AnyUpdate {
+        return AnyUpdate(
+            schema: Model.anySchema,
+            predicate: predicate?.expression,
+            valueSet: valueSet.valueSet
+        )
     }
 }
 
@@ -22,10 +64,6 @@ extension Update: Hashable {
 
 extension Update {
     internal func makeSQL() -> SQL.Update {
-        return SQL.Update(
-            table: SQL.Table(Model.schema.name),
-            values: valueSet.makeSQL(),
-            predicate: predicate?.expression.sql
-        )
+        return update.makeSQL()
     }
 }
