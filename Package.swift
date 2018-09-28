ccbc4d037f5e8b6cd234632ac7296db70a3c0e0f
Matt Diephouse <matt@diephouse.com>
2018-09-28 14:30:30 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,9 +11,9 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/PersistX/Schemata.git", from: "0.1.2"),
+        .package(url: "https://github.com/PersistX/Schemata.git", from: "0.2.0"),
         .package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", from: "4.0.0"),
-        .package(url: "https://github.com/tonyarnold/Differ.git", from: "1.2.3"),
+        .package(url: "https://github.com/tonyarnold/Differ.git", from: "1.3.0"),
     ],
     targets: [
         .target(
