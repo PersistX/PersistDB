0760f62667312429eb66564e923f8a0078ac3663
Matt Diephouse <matt@diephouse.com>
2019-03-31 10:29:16 -0400

    Update to Schemata 0.3.1

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/PersistX/Schemata.git", from: "0.3.0"),
+        .package(url: "https://github.com/PersistX/Schemata.git", from: "0.3.1"),
         .package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", from: "5.0.0"),
         .package(url: "https://github.com/tonyarnold/Differ.git", from: "1.4.0"),
     ],
